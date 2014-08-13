a346e0f99077f9f2d245b1b627c66179267c8c04
Willi Schönborn <w.schoenborn@gmail.com>
2014-08-13 02:02:31 +0200

    Added stub for overloaded range value-of method

--- core/src/main/java/com/codereligion/versions/VersionRange.java
+++ core/src/main/java/com/codereligion/versions/VersionRange.java
@@ -8,7 +8,11 @@ public final class VersionRange {
         // static factory
     }
     
-    public static Range<Version> valueOf(final String range) {
+    public static Range<Version> valueOf(String range) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public static Range<Version> valueOf(String range, VersionPrecedence precedence) {
         throw new UnsupportedOperationException();
     }
     
